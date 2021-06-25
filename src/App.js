c93cf026eafc4f9b198e8b76ef2329266e81fdba
Umair Zafar <umairzafar164@gmail.com>
2021-06-25 12:09:55 +0500

    just for testing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+class App extends React.Component {
+  state = { city: "", temp: null, cities: [], temps: [] };
+  onSubmit = () => {
+    this.setState({
+      cities: [...this.state.cities, this.state.city],
+      temps: [...this.state.temps, this.state.temp],
+      city: "",
+      temp: "",
+    });
+  };
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.city}
+          onChange={(e) => this.setState({ city: e.target.value })}
+        ></input>
+        <br></br>
+        <input
+          value={this.state.temp}
+          onChange={(e) => this.setState({ temp: e.target.value })}
+        ></input>
+        <button onClick={this.onSubmit}>Submit</button>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <table class="ui celled table">
+          <thead>
+            <tr>
+              <th>City</th>
+              <th>Temperature</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              {this.state.cities.map(m=><td>{m}</td>)}
+              {this.state.temps.map(t=><td>{t}</td>)}
+              </tr>
+          </tbody>
+        </table>
+      </div>
+      
+    );
+  }
 }
 
 export default App;
+
